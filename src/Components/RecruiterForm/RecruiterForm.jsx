a42dbdf064745d2e3780a828153767842d78f080
Avik Roy <somavik99@gmail.com>
2024-03-14 15:03:06 +0530

    main

--- src/Components/RecruiterForm/RecruiterForm.jsx
+++ src/Components/RecruiterForm/RecruiterForm.jsx
@@ -1,4 +1,7 @@
 import "./RecruiterForm.css";
+import { HiOutlineOfficeBuilding } from "react-icons/hi";
+import { FaHome } from "react-icons/fa";
+import { FaTruckField } from "react-icons/fa6";
 
 const RecruiterForm = () => {
   return (
@@ -26,13 +29,54 @@ const RecruiterForm = () => {
               <label htmlFor="JobTitle" style={{ fontWeight: "800" }}>
                 Job Title <span style={{ color: "red" }}>*</span>
               </label>
-              <input type="text" id="JobTitle" placeholder="Ex.- Data Analyst..." />
+              <input
+                type="text"
+                id="JobTitle"
+                placeholder="Ex.- Data Analyst..."
+              />
             </div>
           </section>
           <section className="Job__type">
-
+            <p style={{ fontWeight: "800" }}>
+              Job Type <span style={{ color: "red" }}>*</span>
+            </p>
+            <div className="types">
+              <p>full-time</p>
+              <p>part-time</p>
+              <p>Both(full-time and part-time)</p>
+              <p>Intern</p>
+            </div>
           </section>
-          <section className="Documents__field"></section>
+          <section className="Location__field">
+            <p style={{ fontWeight: "800" }}>
+              Location <span style={{ color: "red" }}>*</span>
+            </p>
+            <div>
+              <p className="Type__options">
+                <HiOutlineOfficeBuilding />
+                <input type="radio" name="" id="" />
+                <p>
+                  Work From Office
+                  <div>Candidate have to work at office location</div>
+                </p>
+              </p>
+              <p>
+                <FaHome />
+                <input type="radio" name="" id="" />
+                <p>
+                  Work From Home
+                  <div>Candidate have flexibility to work from home</div>
+                </p>
+              </p>
+              <p>
+                <FaTruckField />
+                <input type="radio" name="" id="" />
+                <p>
+                  Field Job <div>Candidate have to do the field work</div>
+                </p>
+              </p>
+            </div>
+          </section>
         </div>
       </form>
     </div>
